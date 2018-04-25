b1d99c14f1e4f32f234f10e44537f137ef674210
xuwenfeng <xuwenfeng@01zhuanche.com>
2018-04-25 16:06:10 +0800

    commit

--- src/main/java/com/lym/manager/service/impl/ContractServiceImpl.java
+++ src/main/java/com/lym/manager/service/impl/ContractServiceImpl.java
@@ -3,17 +3,15 @@ package com.lym.manager.service.impl;
 import com.lym.manager.mapper.ContractInfoMapper;
 import com.lym.manager.model.ContractInfo;
 import com.lym.manager.service.ContractService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 @Service
 public class ContractServiceImpl implements ContractService {
 
-    @Autowired
+    @Resource
     private ContractInfoMapper mapper;
 
     @Override
